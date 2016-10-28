d8e5fb18491cd6d79570724d7177157af6f35940
Zachary Richards <zachary.j.richards@gmail.com>
2016-10-28 09:39:56 -0700

    add ability to add and delete new questions

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion',true);
+    },
+
+    saveQuestion() {
+      var params = {
+        author: this.get('author'),
+        note: this.get('note'),
+        content: this.get('content')
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('saveQuestion', params);
+    }
+  }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,17 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('question');
   },
+
+  actions: {
+    saveQuestion(params) {
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
+      this.transitionTo('index');
+    },
+
+    destroyQuestion(question) {
+      question.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
 });
