cb7cb48ee3d557929432522bd51311de47046b9f
Zachary Richards <zachary.j.richards@gmail.com>
2016-10-28 11:34:18 -0700

    add dynamic routing in order to view, edit, and delete each question individually

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,28 +3,5 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('question');
-  },
-
-  actions: {
-    saveQuestion(params) {
-      var newQuestion = this.store.createRecord('question', params);
-      newQuestion.save();
-      this.transitionTo('index');
-    },
-
-    update(question, params) {
-      Object.keys(params).forEach(function(key) {
-        if(params[key]!==undefined) {
-          question.set(key,params[key]);
-        }
-      });
-      question.save();
-      this.transitionTo('index');
-    },
-
-    destroyQuestion(question) {
-      question.destroyRecord();
-      this.transitionTo('index');
-    }
   }
 });
--- app/routes/question.js
+++ app/routes/question.js
@@ -4,4 +4,26 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('question', params.question_id);
   },
+  actions: {
+    saveQuestion(params) {
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
+      this.transitionTo('index');
+    },
+
+    update(question, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          question.set(key,params[key]);
+        }
+      });
+      question.save();
+      this.transitionTo('index');
+    },
+
+    destroyQuestion(question) {
+      question.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
 });
